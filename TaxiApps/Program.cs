8bb0996fa32b4d4286e0f7c7ee9e8275891b2692
fafan ramadhan <forweare7@gmail.com>
2022-03-23 21:32:40 +0700

    add and call the method

--- TaxiApps/Program.cs
+++ TaxiApps/Program.cs
@@ -3,5 +3,17 @@ using TaxiApps;
 
 Taxi taksi1 = new Taxi();
 
+//pengesetan nilai properties
+taksi1.Name = "John";
+taksi1.Status = true;
+taksi1.Passenger = 3;
+
+//pemanggilan method
+taksi1.TaxiInfo();
+taksi1.PickUpPassenger();
+taksi1.DropOffPassenger();
+
+Console.ReadKey();
+
 
 
