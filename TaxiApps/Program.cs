7202bd50e7c50a2dba33d286ba856c954d3fcaed
fafan ramadhan <forweare7@gmail.com>
2022-03-23 22:11:37 +0700

    add input driver name

--- TaxiApps/Program.cs
+++ TaxiApps/Program.cs
@@ -1,14 +1,19 @@
 ﻿//membuat objek taksi
 using TaxiApps;
+string Dname;
+
+Console.WriteLine("Input Driver Name: ");
+Dname = Console.ReadLine();
 
 Taxi taksi1 = new Taxi();
 
 //pengesetan nilai properties
-taksi1.Name = "John";
+taksi1.Name = Dname;
 taksi1.Status = true;
 taksi1.Passenger = 3;
 
 //pemanggilan method
+Console.Clear();
 taksi1.TaxiInfo();
 taksi1.PickUpPassenger();
 taksi1.DropOffPassenger();
